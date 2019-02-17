dfa290f87ba96a2c82f3f210f90fcab25dea2127
Felix Pojtinger <felix@pojtinger.com>
2019-02-17 18:38:46 +0100

    feat(provisoner): Add minio to ipxe

--- packages/provisioner/packages/ipxe-worker/src/lib.js
+++ packages/provisioner/packages/ipxe-worker/src/lib.js
@@ -1,14 +1,95 @@
+const { Client } = require("minio");
+const shell = require("shelljs");
+
 module.exports = class {
-  constructor({ downloaddir, workdir }) {
+  constructor({ artifactId, downloaddir, builddir, distdir }) {
+    this.artifactId = artifactId;
     this.downloaddir = downloaddir;
-    this.workdir = workdir;
+    this.builddir = `${builddir}/${this.artifactId}`;
+    this.distdir = `${distdir}/${this.artifactId}`;
+    shell.mkdir("-p", this.downloaddir);
+    shell.mkdir("-p", this.builddir);
+    shell.mkdir("-p", this.distdir);
   }
 
   async download() {}
 
-  async build() {}
+  async build({ driver, extension }) {
+    this.localFilename = `${driver}.${extension}`;
+    this.remoteFilename = `${this.artifactId}/${driver}.${extension}`;
+    return shell.exec(
+      `echo "asdfasfdasdf" > ${this.builddir}/${this.localFilename}`
+    );
+  }
 
-  async package() {}
+  async package() {
+    return shell.cp(
+      `${this.builddir}/${this.localFilename}`,
+      `${this.distdir}/${this.localFilename}`
+    );
+  }
 
-  async upload() {}
+  async upload({ endpoint, port, region, bucket, accessKey, secretKey }) {
+    const client = new Client({
+      endPoint: endpoint,
+      port,
+      accessKey,
+      secretKey,
+      useSSL: false
+    });
+    const download = {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: ["s3:GetBucketLocation", "s3:ListBucket"],
+          Effect: "Allow",
+          Principal: {
+            AWS: ["*"]
+          },
+          Resource: [`arn:aws:s3:::${bucket}`],
+          Sid: ""
+        },
+        {
+          Action: ["s3:GetObject"],
+          Effect: "Allow",
+          Principal: {
+            AWS: ["*"]
+          },
+          Resource: [`arn:aws:s3:::${bucket}/*`],
+          Sid: ""
+        }
+      ]
+    };
+    return new Promise(resolve =>
+      client.bucketExists(bucket, (_, exists) =>
+        exists
+          ? client.fPutObject(
+              bucket,
+              this.remoteFilename,
+              `${this.distdir}/${this.localFilename}`,
+              {
+                "Content-Type": "application/octet-stream"
+              },
+              () =>
+                client.setBucketPolicy(bucket, JSON.stringify(download), () =>
+                  resolve(true)
+                )
+            )
+          : client.makeBucket(bucket, region, () =>
+              client.fPutObject(
+                bucket,
+                this.remoteFilename,
+                `${this.distdir}/${this.localFilename}`,
+                {
+                  "Content-Type": "application/octet-stream"
+                },
+                () =>
+                  client.setBucketPolicy(bucket, JSON.stringify(download), () =>
+                    resolve(true)
+                  )
+              )
+            )
+      )
+    );
+  }
 };
--- packages/provisioner/packages/ipxe-worker/src/svc.js
+++ packages/provisioner/packages/ipxe-worker/src/svc.js
@@ -9,16 +9,28 @@ module.exports = {
       await job.progress(0);
       const ipxe = new Ipxe({
         downloaddir: "/tmp/ipxe/downloaddir",
-        workdir: `/tmp/ipxe/workdir/${job.data.artifactId}`
+        builddir: `/tmp/ipxe/builddir`,
+        distdir: `/tmp/ipxe/distdir`,
+        artifactId: job.data.artifactId
       });
       await job.progress(10);
       await ipxe.download();
       await job.progress(20);
-      await ipxe.build();
+      await ipxe.build({
+        driver: "ipxe",
+        extension: "lkrn"
+      });
       await job.progress(50);
       await ipxe.package();
       await job.progress(70);
-      await ipxe.upload();
+      await ipxe.upload({
+        endpoint: process.env.MINIO_ENDPOINT,
+        port: parseInt(process.env.MINIO_PORT),
+        region: process.env.MINIO_REGION,
+        bucket: process.env.MINIO_BUCKET,
+        accessKey: process.env.MINIO_ACCESS_KEY,
+        secretKey: process.env.MINIO_SECRET_KEY
+      });
       await job.progress(100);
       return {
         artifactId: job.data.artifactId
