06a75d145fe0d6cc3cfe4c0796801566a332cd19
Felix Pojtinger <felix@pojtinger.com>
2019-02-19 14:18:30 +0100

    feat(provisioner): Put all functionality of the builders into the builders-utils package

--- packages/provisioner/packages/builder-utils/src/index.js
+++ packages/provisioner/packages/builder-utils/src/index.js
@@ -1,1 +1,4 @@
+module.exports.autotools = require("./autotools");
+module.exports.fs = require("./fs");
+module.exports.git = require("./git");
 module.exports.syncQueueWithDb = require("./syncQueueWithDb");
